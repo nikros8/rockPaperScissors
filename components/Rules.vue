cf87efeaa884f002fe99150f0aa778b65733d1b8
nikolas <niko09185@gmail.com>
2024-01-03 20:48:02 +0100

    Rules Modal window on mobile screens is done

--- components/Rules.vue
+++ components/Rules.vue
@@ -1,7 +1,17 @@
-<scrip setup lang="ts"></scrip>
+<script setup lang="ts">
+import rulesSvg from "assets/svg/image-rules.svg"
+const isOpen = ref(false)
+</script>
 <template>
   <div class="rules">
-    <button>RULES</button>
+    <button @click="isOpen = true">RULES</button>
+    <Teleport to="body">
+      <div v-if="isOpen" class="modal">
+        <h1>RULES</h1>
+        <img :src="rulesSvg" alt="Game Rules" />
+        <span @click="isOpen = false" class="material-symbols-outlined"> close </span>
+      </div>
+    </Teleport>
   </div>
 </template>
 <style scope>
@@ -27,5 +37,37 @@
   width: 100%;
   border: 2px solid #888fa9;
   border-radius: 8px;
+}
+
+.modal {
+  background-color: white;
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 92px;
+  padding-right: 35px;
+  padding-bottom: 60px;
+  padding-left: 35px;
+
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  z-index: 999;
+}
+
+.modal h1 {
+  font-size: 32px;
+}
+.modal img {
+  height: auto;
+  max-width: 305px;
+  width: 100%;
+  margin-top: 110px;
+}
+.modal span {
+  color: #d0d0d3;
+  font-size: 32px;
+  margin-top: auto;
 }
 </style>
